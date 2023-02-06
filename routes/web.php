f6d7fd58db374c8726a69b2966e5f4b51de2538e
potechawanakorn <ranimaharani070803@gmail.com>
2023-02-06 15:02:58 +0700

    tampilkan data mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,19 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::post('/jurusan', [JurusanController::class, 'store']);
+Route::middleware('auth')->group(function () {
+    // manajemen jurusan
+    Route::get('/jurusan', [JurusanController::class, 'index']);
+    Route::get('/jurusan/form', [JurusanController::class, 'create']);
+    Route::post('/jurusan', [JurusanController::class, 'store']);
     Route::get('/jurusan/edit/{id}', [JurusanController::class, 'edit']);
     Route::put('/jurusan/{id}', [JurusanController::class, 'update']);
     Route::delete('/jurusan/{id}', [JurusanController::class, 'destroy']);
+
+    Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+    Route::get('/mahasiswa/form', [MahasiswaController::class, 'create']);
+    Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
+    Route::get('/mahasiswa/edit/{id}', [MahasiswaController::class, 'edit']);
+    Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
+    Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy']);
+});
